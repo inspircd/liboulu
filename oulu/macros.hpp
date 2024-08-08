f154d2eea54b134c924d0705955010c54b183b3b
Sadie Powell <sadie@witchery.services>
2024-08-08 13:22:39 +0100

    Add the OULU_ATTR_NOT_NULL macro.

--- oulu/macros.hpp
+++ oulu/macros.hpp
@@ -3,6 +3,17 @@
 
 #pragma once
 
+/** \def OULU_ATTR_NOT_NULL(...)
+ * Enables the compile-time checking of arguments that must never be be null. If a function is
+ * marked with this attribute then the compiler will warn if a null pointer is passed to any of the
+ * specified arguments.
+ */
+#ifdef __GNUC__
+# define OULU_ATTR_NOT_NULL(...) __attribute__((nonnull(__VA_ARGS__)))
+#else
+# define OULU_ATTR_NOT_NULL(...)
+#endif
+
 /** \def OULU_INTERNAL
  * Allows members to be defined as internal-only. This means that they are public for the core
  * library and for tests but private for library consumers.
